0758d10fe972bfc7a6165ee230f141c944014805
Pavel Srytr <pajasry@gmail.com>
2021-06-16 21:08:22 +0200

    Texts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ var http = require('http');
 //create a server object:
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(req.url);
-    res.write('Hello World!'); //write a response to the client
-    res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+    res.write('Hello World!');
+    res.end();
+}).listen(8080);
