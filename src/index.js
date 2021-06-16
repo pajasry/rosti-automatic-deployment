6d74bf7ac431ce6fbac83a8ade7172c9a6b42a0c
Pavel Srytr <pajasry@gmail.com>
2021-06-16 21:17:58 +0200

    Workflow changes

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('TEST');
+    res.end();
+}).listen(8080);
