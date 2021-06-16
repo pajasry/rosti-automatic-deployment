3ddb8fe27121ef2299dcb316794fe71f1adb230b
Pavel Srytr <pajasry@gmail.com>
2021-06-16 21:12:20 +0200

    Text

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ var http = require('http');
 //create a server object:
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
+    res.write('OSTRÁ');
     res.end();
 }).listen(8080);
