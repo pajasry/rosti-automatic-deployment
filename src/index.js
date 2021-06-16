64116fb59ee43850d48444265a4453a28a88d958
Pavel Srytr <pajasry@gmail.com>
2021-06-16 21:37:11 +0200

    Final refactor

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ var http = require('http');
 //create a server object:
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('TEST');
+    res.write('Working deployment to test env');
     res.end();
 }).listen(8080);
